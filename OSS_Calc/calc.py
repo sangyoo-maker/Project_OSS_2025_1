dc1eda54fd4b5ec30692047b2bc23fa8882f2eff
Sangyoo <sangyu765@gmail.com>
2025-06-04 15:41:17 +0900

    [기능추가] 에러 발생 시 에러 발생 원인을 출력하는 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -5,7 +5,7 @@ class Calculator:
     def __init__(self, root):
         self.root = root
         self.root.title("계산기")
-        self.root.geometry("300x400")
+        self.root.geometry("400x500")
 
         self.expression = ""
 
@@ -19,7 +19,7 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['mod','=']
         ]
 
         for row in buttons:
@@ -39,9 +39,17 @@ class Calculator:
             self.expression = ""
         elif char == '=':
             try:
+                expr=self.expression.replace('mod','%')
                 self.expression = str(eval(self.expression))
+            except ZeroDivisionError: #0으로 나눌 시 에러코드 표시
+                self.expression='0으로 나눌 수 없어요'
             except Exception:
-                self.expression = "에러"
+                if self.expression and self.expression[-1] in ('/','*','-','+','d'):#수식으로 끝날 시 에러코드 표시
+                    self.expression="에러:완성되지 않은 수식"
+                elif self.expression=='':
+                    self.expression=''
+                else:
+                    self.expression = "에러"
         else:
             self.expression += str(char)
 
